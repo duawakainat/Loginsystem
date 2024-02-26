3d66c7c55a592ee716e4b1ee1f80dc3716ab7f02
kainat kainat <kainathafeez04@gmail.com>
2024-02-26 05:37:05 -0800

    change

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 export default async function page() {
-  const session = await getServerSession(authOptions);s
+  const session = await getServerSession(authOptions);
   if (session) redirect("/dashboard");
   return (
           <Form />
